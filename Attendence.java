c19d251fd5044b0a77ec3e1e98c8a596a34e43d5
wangtim9 <wangtim27@7hills.org>
2025-11-19 09:35:46 -0500

    Add files via upload
    added variables and started to make a constructor

---
+++ Attendence.java
@@ -0,0 +1,22 @@
+public class Attendence {
+	int numID;
+	String nameLast;
+	String nameFirst;
+	String companyID;
+	Table
+	Seat
+	
+	public 
+	
+	public class Attendee {
+		File Guests = new File(confGuests.txt);
+		
+		public Attendee(int number, String filename) {
+			public int readFile() throws IOException {
+				File myFile = new File(filename);
+				Scanner scan = new Scanner(myFile);
+				AttendeeInfo[i] = scan.nextLine();
+			}
+			System.out.println();
+			scan.close();
+		}

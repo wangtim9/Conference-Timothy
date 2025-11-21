9434d8c725fb64bf3e6419f25fe74b97d56fc0f5
wangtim9 <wangtim27@7hills.org>
2025-11-21 09:36:44 -0500

    Update Conference.java
    wow guests

--- Conference.java
+++ Conference.java
@@ -1,6 +1,16 @@
+import java.io.*;
+import java.util.*;
+
 public class Conference {
-	int numTables;
+	int numTables=10;
 	int numCompanies;
 	Attendee[150];
-	int PplPerTable
+	int PplPerTable=10;
+	public int Place(String guestfile) {
+		File F = new File(file);
+		Scanner scan = new Scanner(F);
+		for (int i = 0; i<numTables; i++) {
+			for (int j = 0; j<PplPerTable;j++) {
+			
+				if (
 	

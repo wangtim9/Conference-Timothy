14f37549bf542324da9286ba940848873f515707
wangtim9 <wangtim27@7hills.org>
2025-11-24 22:51:41 -0500

    Update Conference.java

--- Conference.java
+++ Conference.java
@@ -12,40 +12,33 @@ public class Conference {
 	}
 
 	public void loadData() {
-		File G = new File("confGuests.txt");
-		Scanner scan = new Scanner(G);
+		try {
+            File file = new File("confGuests.txt");
+            Scanner scan = new Scanner(file);
 
-		int x = 0;
-		while (scan.hasNextLine()) {
-			String data = scan.nextLine();
-			String[] person = data.split(",");
+            int x = 0;
+            while (scan.hasNextLine()) {
+                String data = scan.nextLine();
+                String[] person = data.split(",");
 
-			int id = person[0];
-			String Last = person[1];
-			String First = person[2];
-			int company = person[3];
+                int id = Integer.parseInt(person[0]);
+				String Last = person[1];
+				String First = person[2];
+				int company = Integer.parseInt(person[3]);
 
-			Attendee a1 = new Attendee(id, Last, First, company);
+				Attendee a1 = new Attendee(id, Last, First, company);
 
-			arr[x] = a1;
-			x++;
-		}
+				arr[x] = a1;
+				x++;
+            }
+            scan.close();
 
-		scan.close();
-		numCompanies = x;
+        } catch (FileNotFoundException e) {
+            System.out.println("ERROR: confGuests.txt not found.");
+        }
 	}
-}
-
-public Attendee findAttendee() {
-	for (Attendee a : arr) {
-		if (a != null && a.getID() == id) {
-			return a;
-		}
-	}
-	return null;
-}
-
-public void manual() {
+	
+	public void manual() {
 	Scanner scan = new Scanner(System.in);
 
 	System.out.println("Choose an option:");
@@ -61,12 +54,14 @@ public void manual() {
 				System.out.println(arr[i]);
 			}
 		}
+	}
 	else if (choice == 2) {
-		System.out.println("Enter ID: ")
+		System.out.println("Enter ID: ");
 		int ID = scan.nextInt();
 	
 		System.out.println("Enter first name: ");
 		String First = scan.nextLine();
+		scan.nextLine();
 
 		System.out.println("Enter last name: ");
 		String Last = scan.nextLine();
@@ -77,13 +72,12 @@ public void manual() {
 		for (int i = 0; i < arr.length; i++) {
 			if (arr[i] == null) {
 				arr[i] = new Attendee(ID, Last, First, coID);
-				System.out.println("New attendee added.");
 			}
 		}
+		System.out.println("New attendee added.");
+	}
 	}
 }
-		
-
 
 
 

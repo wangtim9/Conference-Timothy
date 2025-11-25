8a2ba9763cba646dc6e6a17d67550115dd824900
wangtim9 <wangtim27@7hills.org>
2025-11-24 20:34:42 -0500

    Update and rename Attendence.java to Attendee.java

---
+++ Attendee.java
@@ -0,0 +1,17 @@
+public class Attendee {
+	int numID;
+	String nameLast;
+	String nameFirst;
+	int companyID;
+	int Table;
+	int Seat;
+	public Attendee(int ID, String Last, String First, int CompanyID) {
+		numID = ID;
+		nameLast = Last;
+		nameFirst = First;
+		companyID = CompanyID;
+	}
+}
+
+
+

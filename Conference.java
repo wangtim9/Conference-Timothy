119392f4f1427609a843b3469fba7545d3be7aa1
wangtim9 <wangtim27@7hills.org>
2025-11-24 20:53:54 -0500

    Update Conference.java

--- Conference.java
+++ Conference.java
@@ -4,13 +4,11 @@ import java.util.*;
 public class Conference {
 	int numTables=10;
 	int numCompanies;
-	Attendee[150];
+	Attendee[] arr = Attendee[150];
 	int PplPerTable=10;
-	public int Place(String guestfile) {
+	public Conference() {
+		
 		File F = new File(file);
 		Scanner scan = new Scanner(F);
-		for (int i = 0; i<numTables; i++) {
-			for (int j = 0; j<PplPerTable;j++) {
-			
-				if (
 	
+

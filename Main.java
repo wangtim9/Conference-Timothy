07cc954fef40117067250790d76ef9f4e52564a1
wangtim9 <wangtim27@7hills.org>
2025-11-24 21:18:36 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
   public static void main (String[] args){
+    Scanner scan = new Scanner(System.in);
     Conference c1 = new Conference();
+
+    c1.loadData("confGuests.txt");
+
+    c1.manual();
   }
 }

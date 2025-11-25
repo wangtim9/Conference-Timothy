1517723f218202734dbee6d42b94db0bfcba6e33
wangtim9 <wangtim27@7hills.org>
2025-11-24 22:58:01 -0500

    Update Main1.java

--- Main1.java
+++ Main1.java
@@ -1,7 +1,7 @@
 public class Main1 {
   public static void main (String[] args) {
-    Conference c1 = new Conference();
-    c1.loadData();
-    c1.manual();
+    Conference c1 = new Conference();//make a new conference
+    c1.loadData();//load file into 1D array of the attendee objects
+    c1.manual();//prompts user
   }
 }

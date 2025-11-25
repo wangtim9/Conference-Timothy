73dced9057f98cad308761d371ca1e9e5d08e82a
wangtim9 <wangtim27@7hills.org>
2025-11-24 21:45:41 -0500

    Update Conference.java

--- Conference.java
+++ Conference.java
@@ -20,8 +20,39 @@ public class Conference {
 			String data = scan.nextLine();
 			String[] person = data.split(",");
 
-			person[0].getID;
-		
+			int id = person[0].getID;
+			String First = person[1].getFirst;
+			String Last = person[2].getLast;
+			int company = person[3].getComp;
+
+			Attendee a1 = newAttendee(id, First, Last, company);
+
+			arr[x] = a1;
+			x++;
+		}
+
+		scan.close();
+		numCompanies = x;
+	}
+}
+
+public void listAttendees() {
+	for (Attendee a : arr) {
+		if (a != null) {
+			System.out.println(a):
+		}
+	}
+}
+
+public Attendee findAttendee() {
+	for (Attendee a : arr) {
+		if (a != null && a.getID() == id) {
+			return a;
+		}
+	}
+	return null;
+}
 	
+
 
 

6eb64e90cc59f6cf8c64c3101e4bd4e4ac1948f5
wangtim9 <wangtim27@7hills.org>
2025-11-24 21:39:28 -0500

    Update Conference.java

--- Conference.java
+++ Conference.java
@@ -4,11 +4,24 @@ import java.util.*;
 public class Conference {
 	int numTables=10;
 	int numCompanies;
-	Attendee[] arr = Attendee[150];
+	Attendee[] arr = new Attendee[150];
 	int PplPerTable=10;
+
 	public Conference() {
+		loadData();
+	}
+
+	public void loadData() {
+		File G = new File("confGuests.txt");
+		Scanner scan = new Scanner(G);
+
+		int x = 0;
+		while (scan.hasNextLine()) {
+			String data = scan.nextLine();
+			String[] person = data.split(",");
+
+			person[0].getID;
 		
-		File F = new File(file);
-		Scanner scan = new Scanner(F);
 	
+
 
